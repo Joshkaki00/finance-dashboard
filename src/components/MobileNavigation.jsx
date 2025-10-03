d28dcb0bd27717bc55c11a34e858ba1c38b85941
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-10-02 18:16:32 -0700

    updated

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -37,7 +37,7 @@ const MobileNavigation = () => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
         </svg>
       ),
-      component: <TransactionForm />
+      component: <MobileTransactionForm />
     },
     {
       id: 'budget',
