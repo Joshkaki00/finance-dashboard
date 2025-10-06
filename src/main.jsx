94afe21c0a75d9dc6aeed523426ff5b672e49a1c
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-10-06 01:49:14 -0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App'
 import './index.css'
+import { measurePerformance, preloadCriticalResources, registerServiceWorker } from './utils/performance'
 
 // Add app loaded class to hide loading spinner
 const root = ReactDOM.createRoot(document.getElementById('root'));
